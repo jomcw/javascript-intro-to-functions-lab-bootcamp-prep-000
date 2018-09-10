a9fe660264ee692d5589712b2edd4d806f9fc106
John McWatters <jo.a.mcw@gmail.com>
2018-09-10 03:39:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,15 @@ function logWhisper(string) {
   console.log(whisper(string))
 }
 
+function sayHiToGrandma(string){
+  if(string === string.toLowerCase)
+  return "I can't hear you!"
+  
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+}
